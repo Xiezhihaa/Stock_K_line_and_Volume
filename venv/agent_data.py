50fc1421ae991d14be804f2d91a41a458d6c682b
Xiezhihaa <48302367+Xiezhihaa@users.noreply.github.com>
2019-11-13 17:26:56 +0800

    Update agent_data.py

--- venv/agent_data.py
+++ venv/agent_data.py
@@ -3,12 +3,15 @@ import time
 import pandas as pd
 import selenium.webdriver.support.ui as ui
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 
 def into_website(url,url1):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
     # 開啟瀏覽器
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     # 登入網站
     browser.get(url1)
     # 等待時間
@@ -35,21 +38,35 @@ def into_website(url,url1):
     # 進入目標網頁
     browser.get(url)
 
-def get_buy():
+    return browser
+
+def get_buy(browser):
     tablet_buy = browser.find_element_by_xpath("/html/body/div[4]/div[3]/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/table")
     trlist_buy = tablet_buy.find_elements_by_tag_name('tr')
     # [券商名稱,買進,賣出,買超,均價]
     global agent_buy
     agent_buy = []
 
-    for row in trlist_buy:
-        tdlist = row.find_elements_by_tag_name('td')
-        arr = []
-        for col in tdlist:
-            arr.append(col.text)
-        agent_buy.append(arr)
+    for i in range(len(trlist_sell)):
+        a=str(i+1)
+        xpath='/html/body/div[4]/div[3]/div[2]/div[2]/div[3]/div[3]/div[1]/div[2]/table/tbody/tr['+a+']/td[1]/a'
+        browser.find_element_by_xpath(xpath).click()
+        time.sleep(20)
+        browser.find_element_by_xpath('/html/body/div[4]/div[3]/div[2]/div[2]/div[8]/div[1]/div[3]/div[2]/button[3]').click()
+        table_buy = browser.find_element_by_xpath('//*[@id="DataTables_Table_0"]/tbody')
+        trlist_buy = table_buy.find_elements_by_tag_name('tr')
+        for row in trlist_buy:
+            tdlist = row.find_elements_by_tag_name('td')
+            arr=[]
+            for col in tdlist:
+                arr.append(col.text)
+            agent_buy.append(arr)
+        browser.get(url)
+        time.sleep(20)
 
-def get_sell():
+    return agent_buy
+
+def get_sell(browser):
     # 獲取表格數據
     tablet_sell = browser.find_element_by_xpath("/html/body/div[4]/div[3]/div[2]/div[2]/div[3]/div[3]/div[2]/div[2]/table")
     # 獲取總行數
@@ -58,6 +75,9 @@ def get_sell():
     global agent_sell
     agent_sell = []
 
+    global agent
+    agent=[]
+
     for row in trlist_sell:
         tdlist = row.find_elements_by_tag_name('td')
         arr = []
@@ -65,9 +85,12 @@ def get_sell():
             arr.append(col.text)
         agent_sell.append(arr)
 
+    return agent_sell
+
+#資料庫
 def get_conn_db():
     try:
-        db = pymysql.connect(host='127.0.0.1',user='root',passwd='',port=3306,charset='utf8',db='')
+        db = pymysql.connect(host='127.0.0.1',user='root',passwd='404220008',db="stock_test",port=3306,charset='utf8mb4')
     except pymysql.Error as e:
         print(e)
         print("連接資料庫失敗")
@@ -84,7 +107,7 @@ def add_data():
     sql = 'INSERT INTO'
     try:
         db.get_conn_db()
-        sursor = db.conn.cursor()
+        cursor = db.conn.cursor()
         cursor.execute(sql,)
         db.conn.commit()
         return 1
@@ -100,13 +123,14 @@ def add_data():
         cursor.close()
         db.close_conn_db()
 
-
-
+#執行
 stockID = input("請輸入你要查詢的股票代號")
 date = input("請輸入欲查詢起始時間，格式如2019-09-01")
 url1 = "https://www.nvesto.com/"
 url = "https://www.nvesto.com/tpe/"+stockID+"/majorForce#!/fromdate/"+date+"/todate/"+date+"/view/summary"
 
-into_website(url,url1)
-get_buy()
-get_sell()
+browser=into_website(url,url1)
+agent_buy = get_buy(browser)
+agent_sell = get_sell(browser)
+
+#[日期,買進,比例,賣出,比例,買賣超,近一週.....]
